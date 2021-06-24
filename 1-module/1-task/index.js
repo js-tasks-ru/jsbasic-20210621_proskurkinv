725d65491a8b6978b29ed9e1a9c1015908cb985d
FilinCold <proskurkinv@mail.ru>
2021-06-25 00:30:38 +0300

    All tasks completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  let rezult = 1;
+  if(n === 0 || n === 1) {
+    return 1;
+  }
+  for (let i = n; i >=1; i--) {
+    rezult *= i;
+  }
+  return rezult;
 }
--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,14 @@
 function ucFirst(str) {
-  // ваш код...
+  if(str == '') {
+    return str;
+  }
+  else if (str.length == 1) {
+    return str[0].toUpperCase();
+  }
+  else {
+    const firstSymbol = str.slice(0,1).toUpperCase();
+    const twoPartStr = str.slice(1);
+    str = firstSymbol + twoPartStr;
+    return str;
+  }
 }
