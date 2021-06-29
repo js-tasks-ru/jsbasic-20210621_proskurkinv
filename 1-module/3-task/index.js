1a306c86a9301323e070a679928958d15f5d5b0f
FilinCold <proskurkinv@mail.ru>
2021-06-29 20:04:16 +0300

    Fixed Vlad marks

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,12 +1,7 @@
 function ucFirst(str) {
   if (!str) {
     return str;
-  } else if (str.length == 1) {
-    return str[0].toUpperCase();
   }
-  const firstSymbol = str.slice(0, 1).toUpperCase();
-  const twoPartStr = str.slice(1);
-  str = firstSymbol + twoPartStr;
-  return str;
-
+  str.length == 1 ? str[0].toUpperCase() : false;
+  return str[0].toUpperCase() + str.substring(1);
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,4 @@
 function checkSpam(str) {
   const newStr = str.toLowerCase();
-  if (newStr.includes('1xbet') || newStr.includes('xxx')) {
-    return true;
-  }
-  return false;
+  return newStr.includes('1xbet') || newStr.includes('xxx');
 }
