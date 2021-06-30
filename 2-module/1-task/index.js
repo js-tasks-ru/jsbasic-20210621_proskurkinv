90768b027bdbb39bc060f436adf215c803d79559
FilinCold <proskurkinv@mail.ru>
2021-06-30 19:48:16 +0300

    Completed 2module tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let rezult = 0;
+  for (const key in salaries) {
+    if (salaries[key] == Infinity || salaries[key] == -Infinity || isNaN(salaries[key])) {
+      continue;
+    }
+    if (typeof salaries[key] === 'number') {
+      rezult += salaries[key]
+    }
+  }
+  return rezult;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,10 @@
 function isEmpty(obj) {
-  // ваш код...
+  let count = 0;
+  for (const key in obj) {
+    count++;
+  }
+  if (count == 0) {
+    return true;
+  }
+  return !(count > 0);
 }
